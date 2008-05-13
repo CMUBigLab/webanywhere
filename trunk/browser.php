8479dc2fd78a3112961f2cb4ab0cfc3c3dcf051c
jeffrey.bigham <jeffrey.bigham@22892503-ae3b-0410-aca1-c90317534331>
2008-05-13 22:36:56 +0000

    Cleans up/unifies the way multiple scripts are included.

--- trunk/browser.php
+++ trunk/browser.php
@@ -1,33 +1,63 @@
+<?php session_start(); ?>
 <?php include('config.php'); ?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-  <head>
-    <title>Browser Control Window</title>
-      <script type="text/javascript">
-    <!--
-    top.webanywhere_url=String(document.location).replace(/^(https?:\/\/[^\/]*)\/.*$/, '$1')+'<?php echo $root_path; ?>';
-    top.sound_url_base='<?php echo $sound_url_base; ?>';
-    top.web_proxy_url='<?php echo $wp_path; ?>';
-    if(hasConsole) console.log(top.sound_url_base + ' ' + top.web_proxy_url);
-    -->
+<head>
+<title>WebAnywhere Browser Frame</title>
+<script type="text/javascript">
+/* <![CDATA[ */
+var hasConsole = (typeof console != 'undefined' && typeof console.log != 'undefined');
+top.webanywhere_domain='<?php echo $webanywhere_domain; ?>';
+top.webanywhere_location = String(document.location).replace(/^(https?:\/\/[^\/]*)\/.*$/, '$1');
+top.webanywhere_url=top.webanywhere_location+'<?php echo $root_path; ?>';
+if(hasConsole) console.log(top.sound_url_base + ' ' + top.web_proxy_url);
+top.sound_url_base='<?php echo $sound_url_base; ?>';
+top.web_proxy_url='<?php echo $wp_path; ?>';
+top.cross_domain_security = <?php echo $cross_domain_security; ?>;
+/* ]]> */
 </script>
+<script language="Javascript" src="<?php echo $script_path; ?>/keymapping.php"></script>
 <?php
 // It's about a million times easier to debug Javascript when your source files
 // haven't been messed with.  Unfortunately, it's also slower and causes the
 // browser to issue many more requests.
+if($_REQUEST[embed]!=='true') { ?>
+<?php
+}
+
+// Array of scripts used by the system.
+$scripts =
+  array('/utils.js',
+        '/base64.js',
+        '/handlenodes.js',
+        '/wa.js',
+        '/sounds.js',
+        '/keyboard.js');
+
+// Depending on the type of sound player used, include the appropriate file.
+if($_REQUEST[embed]==='true') {
+  array_unshift($scripts, '/sound_embed.js');
+} else {
+  array_unshift($scripts, '/soundmanager2.js');
+}
+
+// Depending on whether we're in debug mode, either include
+// each script separately (better for debugging), or
+// combined script using the script minimizer.
 if($_REQUEST[debug]==='true') {
+  $start = '<script language="Javascript" src="/' . $script_path;
+  $end = '"></script>';
+  
+  // Output script tags individually.
+  echo $start . implode($end . "\n" . $start, $scripts) . $end . "\n";
+} else {
+  echo '<script language="Javascript" src="';
+  echo $min_script_path . '/scripts.php?files=';
+
+  // Concatenate the individual scripts used into one long string.
+  echo $script_path . implode(',' . $script_path, $scripts) . '"></script>';
+}
 ?>
-<script src="<?php echo $script_path; ?>/wa.js"></script>
-<script src="<?php echo $script_path; ?>/helper.js"></script>
-<script src="<?php echo $script_path; ?>/soundmanager2.js"></script>
-<script src="<?php echo $script_path; ?>/sounds.js"></script>
-<script src="<?php echo $script_path; ?>/handlenodes.js"></script>
-<script src="<?php echo $script_path; ?>/keyboard.js"></script>
-<script src="<?php echo $script_path; ?>/base64.js"></script>
-<script src="<?php echo $script_path; ?>/utils.js"></script>
-<script scr="<?php echo $script_path; ?>/sound_embed.js"></script>
-<?php } else { ?>
-<script src="<?php echo $min_script_path; ?>/scripts.php?files=<?php echo $script_path; ?>/helper.js,<?php echo $script_path; ?>/soundmanager2.js,<?php echo $script_path; ?>/sounds.js,<?php echo $script_path; ?>/handlenodes.js,<?php echo $script_path; ?>/wa.js,<?php echo $script_path; ?>/sound_embed.js,<?php echo $script_path; ?>/base64.js,<?php echo $script_path; ?>/utils.js,<?php echo $script_path; ?>/keyboard.js"></script>
-<?php } ?>
 
 <style>
 body {
@@ -36,9 +66,9 @@ body {
 #input { font-size: 2em; }
 #body { font-family: arial; }
 </style>
+
 </head>
 <body onload="focusLocation(); prefetchLetters();" bgcolor="#CCCCFF">
-<script type="text/javascript">soundManager.createMovie();</script>
 <div align="center" valign="bottom" style="font-size: 1em;">
 <form onSubmit="javascript:navigate(this);return false;">
 <label for="location">Location</label>: <input type="text" size="50" id="location" value="http://webinsight.cs.washington.edu/wa/content.php"/>
@@ -53,24 +83,13 @@ body {
 <div <?php if($_REQUEST[debug] === 'true') { echo 'style="visibility: display;"'; } else { echo 'style="visibility: hidden"'; } ?>>Playing: <span id="playing_div"></span> Features: <span id="sound_div"></span></div>
 <div <?php if($_REQUEST[debug] === 'true') { echo 'style="visibility: hidden;"'; } else { echo 'style="visibility: hidden"'; }?>><span id="test_div"></span></div>
 <div <?php if($_REQUEST[debug] === 'true') { echo 'style="visibility: hidden;"'; } else { echo 'style="visibility: hidden"'; }?>><span id="debug_div"></span></div>
-<?php
-if($_REQUEST[embed]==='true') {
-?>
-<p id='p_embed'>
-<embed src="sounds/blank.mp3" autostart=true hidden=true id="audio_embed" enablejavascript="true" onload="alert('loaded');">
-</p>
-<?php } ?>
-<?php
-//if($_REQUEST[debug]==='true') {
-?>
+<?php if($_REQUEST[debug]==='true') { ?>
 <p>
 <form name="recorder_form" method="post" action="recorder.php"><br/>
 <input name="submit" type="submit" value="submit">
 <textarea id="recording" name="recording" rows="30" cols="150"></textarea>
 </form>
 </p>
-<?php
-//}
-?>
+<?php } ?>
 </body>
 </html>
