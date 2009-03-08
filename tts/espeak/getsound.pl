4a1466b709ffee19930cd541ba3dadae46ea6213
hgneng <hgneng@22892503-ae3b-0410-aca1-c90317534331>
2009-03-08 15:21:34 +0000

    We can use any language eSpeak provide now.

--- tts/espeak/getsound.pl
+++ tts/espeak/getsound.pl
@@ -1,29 +1,51 @@
 #!/usr/bin/perl
 
 use strict;
+use warnings;
+use utf8;
 use CGI qw(:standard);
-use Encode;
+#use Encode;
+#use Encode::CN;
+#use Encode::KR;
+#use Encode::TW;
+#use Encode::Detect::CJK;
 use File::Temp qw/ :POSIX /;
 
-my $base_dir = '/home/hgneng/www-perl';
-my $text;
+my $debuging = 1;
 
-if(!param()) {
-  $text = "There was an error with the request to the speech server.";
-} else {
-  $text = param('text');
-  $text =~ s/\s/ /g;
-  $text =~ s/\"//g;  #"
-  $text =~ s/\s&\s/ and /;
-#  `echo "speaking $text" >> /tmp/espeak.log`;
+# change this directory to where this file (espeak.pl) stores.
+my $base_dir = '/home/hgneng/www-perl';
+
+my $lang = param('lang');
+my $text = param('text');
+
+# handle encoding
+`echo "raw: $text" >> /tmp/espeak.log` if ($debuging);
+#my $charset = Encode::Detect::CJK::detect($text);
+#$text = decode($charset, $text);
+# decode MS %u infamous non-standard encoding in URL
+while ($text =~ /([^%]*)%u(....)(.*)/) {
+  $text = $1 . chr(hex("0x$2")) . $3;
+}
+$text =~ s/\"//g;
+`echo "speaking $text" >> /tmp/espeak.log` if ($debuging);
+
+# set voice
+my $voice = 'en'; # use English as default
+if (lc($lang) eq 'cantonese') {
+  $voice = 'zhy';
+} elsif (lc($lang) eq 'mandarin') {
+  $voice = 'zh';
+} elsif ($lang) {
+  $voice = $lang; # Validation of voice is required in future
 }
 
-my $wav_file = tmpnam() . '.wav';
+# encode mp3 file, use cache in future.
 my $mp3_file = tmpnam() . '.mp3';
-#`echo "tempfile: $wav_file, $mp3_file" >> /tmp/espeak.log`;
-system("espeak -w $wav_file \"$text\" && sync");
-system("$base_dir/lame --preset voice -q 9 --vbr-new $wav_file $mp3_file & sync");
+`echo "tempfile: $mp3_file" >> /tmp/espeak.log`;
+system("espeak -v$voice \"$text\" --stdout | lame --preset voice -q 9 --vbr-new - $mp3_file");
 
+# output mp3 audio stream
 my $buff;
 print "Content-Type: audio/mpeg\n";
 print "Content-Length: " . (-s $mp3_file) . "\n";
