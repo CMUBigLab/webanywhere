b4b40c6a47b86aa7185ed0415ebd564b4adf422e
hgneng <hgneng@22892503-ae3b-0410-aca1-c90317534331>
2009-04-04 16:58:19 +0000

    Add redirection for English

--- tts/ekho/ekho_syllable.pl
+++ tts/ekho/ekho_syllable.pl
@@ -13,15 +13,15 @@ use eGuideDog::Dict::Korean;
 #use Encode::TW;
 #use Encode::Detect::CJK;
 
-my $debuging = 1;
+my $debuging = 0;
 
-# change this directory to where this file (espeak.pl) stores.
-my $base_dir = '/home/hgneng/www-perl';
-
-my $lang = param('lang');
+my $voice = param('voice');
+$voice = 'jyutping' if (not $voice);
+$voice = lc($voice);
 my $text = param('text');
+$text = '' if (not defined $text);
 
-# handle encoding
+# handle encoding, need to be improved...
 `echo "raw: $text" >> /tmp/ekho_syllable.log` if ($debuging);
 #my $charset = Encode::Detect::CJK::detect($text);
 #$text = decode($charset, $text);
@@ -33,44 +33,43 @@ $text =~ s/\"//g;
 
 my $syllable;
 my $dict;
-
-if (lc($lang) eq 'cantonese') {
+if ($voice eq 'jyutping') {
   $dict = eGuideDog::Dict::Cantonese->new();
   $syllable = $dict->get_jyutping($text);
-} elsif (lc($lang) eq 'mandarin') {
+} elsif ($voice eq 'pinyin') {
   $dict = eGuideDog::Dict::Mandarin->new();
   $syllable = $dict->get_pinyin($text);
-} elsif (lc($lang) eq 'korean') {
+} elsif ($voice eq 'hangul') {
   $dict = eGuideDog::Dict::Korean->new();
   $syllable = $dict->get_hangul($text);
 } 
 
-$syllable = '' if (not defined $syllable);
-`echo "speaking $text: $syllable" >> /tmp/ekho_syllable.log` if ($debuging);
+if (not defined $syllable) {
+  # use Festial server as agent for English
+  `echo "speaking $text with Festival" >> /tmp/ekho_syllable.log` if ($debuging);
 
-# output mp3 audio stream
-
-my $mp3_file;
-if ($syllable) {
-  $mp3_file = "/var/www/e-guidedog/htdocs/files/jyutping/$syllable.mp3";
+  # following link may not work in future
+  print redirect("http://webanywhere.cs.washington.edu/cgi-bin/getsound.pl?text=$text&cache=1&mtts=1");
 } else {
-  $mp3_file = "/var/www/e-guidedog/htdocs/files/jyutping/null.mp3";
-}
+  `echo "speaking $text: $syllable" >> /tmp/ekho_syllable.log` if ($debuging);
+  # output mp3 audio stream
+  my $mp3_file = "../htdocs/files/$voice/$syllable.mp3";
+  `echo "play $mp3_file" >> /tmp/ekho_syllable.log` if ($debuging);
 
-`echo "play $mp3_file" >> /tmp/ekho_syllable.log` if ($debuging);
-
-my $buff;
-print "Content-Type: audio/mpeg\n";
-print "Content-Length: " . (-s $mp3_file) . "\n";
-print "Final-name: " . $mp3_file . "\n";
-if ($debuging) {
-  print "Expires: 0\n\n";
-} else {
-  print "Expires: Tue, 12 Mar 2012 04:00:25 GMT\n\n"; 
+  my $buff;
+  print "Content-Type: audio/mpeg\n";
+  print "Content-Length: " . (-s $mp3_file) . "\n";
+  print "Final-name: " . $mp3_file . "\n";
+  if ($debuging) {
+    print "Expires: 0\n\n";
+  } else {
+    # update this date half a year
+    print "Expires: Tue, 30 Jun 2009 23:59:59 GMT\n\n"; 
+  }
+  open(FILE, $mp3_file);
+  while(my $re = read(FILE, $buff, 4096)) {
+    my $total_re += $re;
+    print $buff;
+  }
+  close(FILE);
 }
-open(FILE, $mp3_file);
-while(my $re = read(FILE, $buff, 4096)) {
-  my $total_re += $re;
-  print $buff;
-}
-close(FILE);
