9551c6fa1b959e25c23283b706c14ba1faee22db
hgneng <hgneng@22892503-ae3b-0410-aca1-c90317534331>
2010-01-17 06:52:19 +0000

    add German locale (I know very little German. The translation needs native speaker to review)

---
+++ locale/de/LC_MESSAGES/WebAnywhere.js
@@ -0,0 +1,54 @@
+var wa_text = new Array();
+wa_text[""]="";
+wa_text["Go"]="Grasen";
+wa_text["Next"]="Zurück";
+wa_text["Previous"]="Vor";
+wa_text["Welcome to WebAnywhere"]="Willkommen auf webanyhwere";
+wa_text["Invalid key press"]="Ungültige Taste";
+wa_text["Welcome to Web Anywhere"]="Willkommen auf Web Anyhwere";
+wa_text["End of page"]="Ende der Seite";
+wa_text["Page has loaded."]="Seite geladen wurde.";
+wa_text["WebAnywhere has loaded."]="WebAnywhere geladen wurde.";
+wa_text["Headings"]="Überschriften";
+wa_text["Heading"]="Überschrift";
+wa_text["Links"]="Links";
+wa_text["Link"]="Link";
+wa_text["Type a string to find in the current page"]="Geben Sie eine Zeichenfolge in die aktuelle Seite zu finden";
+wa_text["Start of page"]="Beginn der Seite";
+wa_text["Not in a table."]="nicht in einer Tabelle";
+wa_text["no"]="nein";
+wa_text["Location field text area:"]="Textbereich des Standorts";
+wa_text["Select a language to switch to"]="Wählen Sie eine Sprache zu wechseln";
+wa_text["Button"]="Schaltfläche";
+wa_text["Checkbox"]="Kontrollkästchen";
+wa_text["File Input"]="Input-Datei";
+wa_text["Image Input"]="Input-Bild";
+wa_text["Password Textarea"]="Textbereich Passwort";
+wa_text["Radio Button"]="Radio-Button";
+wa_text["Reset Button"]="Reset-Button";
+wa_text["Submit Button"]="Submit-Button";
+wa_text["Text Area"]="Textbereich";
+wa_text["link"]="Link";
+wa_text["button"]="Schaltfläche";
+wa_text["Heading 1"]="Überschrift erste";
+wa_text["Heading 2"]="Überschrift zweite";
+wa_text["Heading 3"]="Überschrift dritte";
+wa_text["Heading 4"]="Überschrift vierte";
+wa_text["Heading 5"]="Überschrift fünfte";
+wa_text["Heading 6"]="Überschrift sechste";
+wa_text["Image"]="Bild";
+wa_text["Selection"]="Auswahl";
+wa_text["Table"]="Tabelle";
+wa_text["start"]="Beginn";
+wa_text["rows"]="Reihen";
+wa_text["columns"]="Spalten";
+wa_text["List with"]="Liste mit";
+wa_text["items"]="Elemente";
+
+function wa_gettext(text) {
+  if (wa_text[text]) {
+    return wa_text[text];
+  } else {
+    return text;
+  }
+}
