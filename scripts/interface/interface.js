ca0bbec8f536fa7bb3bb82698ea3dfe23593de64
hgneng <hgneng@22892503-ae3b-0410-aca1-c90317534331>
2010-03-21 15:38:51 +0000

    add keyboard shortcut translation

--- scripts/interface/interface.js
+++ scripts/interface/interface.js
@@ -193,20 +193,23 @@ WA.Interface = {
   _nodeToReturn: null,
 
   _keyboardHelpString:  "<div id='wa_keyboard_shortcuts'>" +
-                    "<h2 id='wa_keyboard_shortcuts_heading'>Keyboard Shortcuts</h2>" +
-                    "<p>The following keyboard shortcuts are available. Press escape to exit this menu.</p>" +
+                    "<h2 id='wa_keyboard_shortcuts_heading'>" + wa_gettext("Keyboard Shortcuts") + "</h2>" +
+                    "<p>" + wa_gettext("The following keyboard shortcuts are available.") + wa_gettext("Press escape to exit this menu.") + "</p>" +
                     "<ul>" +
-                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL-L</b> - move the cursor to the location box where you can type a URL to visit.</li>" +
-                    "<li style='margin: 0; padding: 0.1em;'><b>Arrow Down</b> - read the next element on the page.</li>" +
-                    "<li style='margin: 0; padding: 0.1em;'><b>Arrow Up</b> - read the previous element on the page.</li>" +
-                    "<li style='margin: 0; padding: 0.1em;'><b>TAB</b> - skip to the next link or form control.</li>" +
-                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL-H</b> - skip to the next heading.</li>" +
-                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL-I</b> - skip to the next input element.</li>" +
-                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL-R</b> - skip to the next row by cell when in a table.</li>" +
-                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL-D</b> - skip to the next column by cell when in a table.</li>" +
-                    "<li style='margin: 0; padding: 0.1em;'><b>Page Down</b> - read continuously from the current position.</li>" +
-                    "<li style='margin: 0; padding: 0.1em;'><b>Home</b> - read continuously, starting over from the beginning of the page.</li>" +
-                    "<li style=' margin: 0; padding: 0.1em;'><b>CTRL</b> - silence WebAnywhere and pause the system.</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL+L</b> - " + wa_gettext("move the cursor to the location box where you can type a URL to visit.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>" + wa_gettext("Arrow Down") + "</b> - " + wa_gettext("read the next element on the page.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>" + wa_gettext("Arrow Up") + "</b> - " + wa_gettext("read the previous element on the page.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>TAB</b> - " + wa_gettext("skip to the next link or form control.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL+H</b> - " + wa_gettext("skip to the next heading.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL+I</b> - " + wa_gettext("skip to the next input element.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL+R</b> - " + wa_gettext("skip to the next row by cell when in a table.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL+D</b> - " + wa_gettext("skip to the next column by cell when in a table.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>Page Down</b> - " + wa_gettext("read continuously from the current position.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>Home</b> - " + wa_gettext("read continuously, starting over from the beginning of the page.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL</b> - " + wa_gettext("silence WebAnywhere and pause the system.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL+" + wa_gettext("Arrow Down") + "</b> - " + wa_gettext("when a selection box is focused, read the next option.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL+" + wa_gettext("Arrow Up") + "</b> - " + wa_gettext("when a selection box is focused, read the previous option.") + "</li>" +
+                    "<li style='margin: 0; padding: 0.1em;'><b>CTRL+F</b> - " + wa_gettext("search text in current page.") + "</li>" +
                     "</ul>" +
                     "</div>",
 
